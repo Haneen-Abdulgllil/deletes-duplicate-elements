80a913bad1342798d9bba91d2b222aa7af056b95
Haneen Abdulglil <Haneenabdulglil2018@yahoo.com>
2022-03-06 21:56:49 -0800

    delete duplicate elements

---
+++ e.php
@@ -0,0 +1,24 @@
+<?php
+
+
+
+
+$givenArray =   array(2,5,2,10,4,5,8,16);
+
+$uniqueArry = array();
+
+foreach($givenArray as $val) { //Loop1 
+    
+    foreach($uniqueArry as $uniqueValue) { //Loop2 
+
+        if($val == $uniqueValue) {
+            continue 2;
+        }
+    }
+    $uniqueArry[] = $val;
+}
+print_r($uniqueArry);
+
+
+
+?>
